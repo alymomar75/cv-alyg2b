dff293780f710711767cfaf4ba9c81fa9d375573
alymomar75 <alymomardiallo75@gmail.com>
2026-02-26 14:01:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ with col_c2:
     st.markdown("""
     <div class="content-box">
         <strong>Langues</strong><br>
-        - Français ★★★★★ Niveau C2<br>
-        - Anglais ★★★☆☆ Niveau B1/B2
+        - Français ★★★★★ (Niveau C2)<br>
+        - Anglais ★★★☆☆ (Niveau B2)
     </div>
     """, unsafe_allow_html=True)
